8532fee60959d331b4f9ed45c4bfbd722018a5d5
Cauê Gallizzi - Gustavo Lucas - Welisson Domingues <caue.paula@aluno.faculdadeimpacta.com.br>
2023-04-06 12:03:48 +0000

    update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Cauê - RA: 2103161\nGustavo - RA:\nWelisson - RA:'
+    return 'Cauê - RA: 2103161 | Gustavo - RA: 2200299 | Welisson - RA: 2200501'
